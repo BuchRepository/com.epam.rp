78690ff7998cf3b6875ade39d461b4624b3b119b
sergiibuchkivskyi <fcmalin@gmail.com>
2025-08-26 23:13:28 +0300

    updated

--- ReportPortal/Tests/FilterTests.cs
+++ ReportPortal/Tests/FilterTests.cs
@@ -45,7 +45,7 @@ public class FilterTests :TestBase
             loginPage.Login(LOGIN, PASSWORD);
             
             Console.WriteLine($"AppContext.BaseDirectory: {AppContext.BaseDirectory}");
-            Console.WriteLine($"Expected path for logfile: {Path.Combine(AppContext.BaseDirectory, "logs/logfile.log")}");
+            Console.WriteLine($"Expected path for logfile: {Path.Combine(AppContext.BaseDirectory, "../../../logs/logfile.log")}");
             /*try
             {
                 string testLogFilePath = Path.Combine(AppContext.BaseDirectory, "logs/test-logfile.log");
