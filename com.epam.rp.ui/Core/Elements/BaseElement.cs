aeed624777d331bfae8c0d9842a3caf2bf28bf75
sergiibuchkivskyi <fcmalin@gmail.com>
2025-10-04 19:27:01 +0300

    module8: added js and actions methods

--- com.epam.rp.ui/Core/Elements/BaseElement.cs
+++ com.epam.rp.ui/Core/Elements/BaseElement.cs
@@ -1,4 +1,5 @@
 using com.epam.rp.core;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 
 namespace com.epam.rp.ui.Core.Elements;
@@ -51,10 +52,61 @@ public class BaseElement
             JsClick();
         }
     }
+    
+    #region JS Executor Methods
+    
+    private IJavaScriptExecutor Js => (IJavaScriptExecutor)Driver;
+    
+    public void ScrollToElement()
+    {
+        Js.ExecuteScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", Element);
+        LoggerService.Info($"Scrolled to element: {Name}");
+    }
+
+    public bool IsScrolledIntoView()
+    {
+        object? result = Js.ExecuteScript(@"
+        const rect = arguments[0].getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    ", Element);
+
+        bool isInView = result is bool b && b;
+
+        LoggerService.Info($"Element '{Name}' is in view: {isInView}");
+        return isInView;
+    }
 
     public void JsClick()
     {
         ((IJavaScriptExecutor)Driver).ExecuteScript("arguments[0].click();", Element);
         LoggerService.Info($"Clicked with JS on: {Name}");
     }
+    
+    #endregion
+    
+    #region Actions Methods
+
+    public void DragAndDropTo(BaseElement target)
+    {
+        var actions = new Actions(Driver);
+        actions.DragAndDrop(Element, target.Element).Perform();
+        LoggerService.Info($"Dragged '{Name}' to '{target.Name}'");
+    }
+
+    public void ResizeByOffset(int offsetX, int offsetY)
+    {
+        var actions = new Actions(Driver);
+        actions.ClickAndHold(Element)
+            .MoveByOffset(offsetX, offsetY)
+            .Release()
+            .Perform();
+        LoggerService.Info($"Resized element '{Name}' by X:{offsetX}, Y:{offsetY}");
+    }
+
+    #endregion
 }
